8d17fc8b1f09f0d9c62c93eb747e1fd1c763dd7c
Shourav23 <137476850+Shourav23@users.noreply.github.com>
2024-07-09 00:18:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    hamburger.addEventListener('click', () => {
+        navbarMenu.classList.toggle('active');
+    });
+});
